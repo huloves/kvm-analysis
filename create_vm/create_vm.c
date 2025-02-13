ae596e4665d55cb534a2985535e525d8016bad05
huloves <17791627852@163.com>
2025-02-13 16:19:22 +0800

    add create_vm dir

--- create_vm/create_vm.c
+++ create_vm/create_vm.c
@@ -44,7 +44,7 @@ int main(int argc, char *argv[])
 
 	vm_fd = ioctl(sys_fd, KVM_CREATE_VM, 0);
 	if (vm_fd < 0) {
-		pr_err("KVM_CREATE_VM ioctl");
+		printf("KVM_CREATE_VM ioctl");
 		ret = vm_fd;
 		goto err_sys_fd;
 	}
